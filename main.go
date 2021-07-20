675ec3dcdbd6e77483306903fa40135b85779e5c
Vladimir Ukalitsky <vovaz04h@gmail.com>
2021-07-20 15:15:40 +0300

    add type post, urls

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+// Post ...
+type Post struct {
+	UserID int    `json:"userId"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
+
+const (
+	userID       = "7"
+	POSTS_URL    = "https://jsonplaceholder.typicode.com/posts?userId="
+	COMMENTS_URL = "https://jsonplaceholder.typicode.com/comments?postId="
+)
+
 func main() {
 }

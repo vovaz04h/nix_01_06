d44d99d5c82bcc201efbe32b2b7768208ec9d094
Vladimir Ukalitsky <vovaz04h@gmail.com>
2021-07-27 11:06:02 +0300

    write posts and comments to adatabase

--- main.go
+++ main.go
@@ -35,17 +35,18 @@ const (
 	POSTS_URL    = "https://jsonplaceholder.typicode.com/posts?userId="
 	COMMENTS_URL = "https://jsonplaceholder.typicode.com/comments?postId="
 
-	ERR_DB_CONNECT     = 1
-	ERR_HTTP_GET       = 2
-	ERR_HTTP_BODY      = 3
-	ERR_JSON_UNMARSHAL = 4
+	ERR_DB_CONNECT = iota + 1
+	ERR_DB_STMT
+	ERR_HTTP_GET
+	ERR_HTTP_BODY
+	ERR_JSON_UNMARSHAL
 )
 
 var (
 	db *sql.DB
 )
 
-func writeCommentToDB(comment Comment, wgComment sync.WaitGroup) {
+func writeCommentToDB(comment Comment, stmtCommentSave *sql.Stmt, wgComment sync.WaitGroup) {
 	// write comments to database
 	err := db.Ping()
 	if err != nil {
@@ -53,13 +54,18 @@ func writeCommentToDB(comment Comment, wgComment sync.WaitGroup) {
 		return
 	}
 
-	// INSERT INTO comments (in, postid, name, email, body) VALUES ()
+	// INSERT INTO comments (id, postid, name, email, body) VALUES ()
+	_, err = stmtCommentSave.Exec(comment.ID, comment.PostID, comment.Name, comment.Email, comment.Body)
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
 
 	wgComment.Done()
 	return
 }
 
-func writePostToDB(post Post, wgPosts sync.WaitGroup) {
+func writePostToDB(post Post, stmtPostSave *sql.Stmt, stmtCommentSave *sql.Stmt, wgPosts sync.WaitGroup) {
 	// write posts to database
 	err := db.Ping()
 	if err != nil {
@@ -68,6 +74,11 @@ func writePostToDB(post Post, wgPosts sync.WaitGroup) {
 	}
 
 	// INSERT INTO posts (id, userid, title, body) VALUES ()
+	_, err = stmtPostSave.Exec(post.ID, post.UserID, post.Title, post.Body)
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
 
 	// get comments with postId
 	resp, err := http.Get(COMMENTS_URL + strconv.Itoa(post.ID))
@@ -75,16 +86,16 @@ func writePostToDB(post Post, wgPosts sync.WaitGroup) {
 		log.Println(err)
 		return
 	}
-
-	// close http.response.body
 	defer resp.Body.Close()
 
+	// read body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
+	// json to struct
 	var comments []Comment
 	err = json.Unmarshal(body, &comments)
 	if err != nil {
@@ -92,12 +103,11 @@ func writePostToDB(post Post, wgPosts sync.WaitGroup) {
 		return
 	}
 
-	var wgComments sync.WaitGroup
-
 	// write comments to database
+	var wgComments sync.WaitGroup
 	for _, comment := range comments {
 		wgComments.Add(1)
-		go writeCommentToDB(comment, wgComments)
+		go writeCommentToDB(comment, stmtCommentSave, wgComments)
 	}
 
 	wgComments.Wait()
@@ -112,8 +122,6 @@ func main() {
 		log.Println(err)
 		//		os.Exit(ERR_DB_CONNECT)
 	}
-
-	// close database connect
 	defer db.Close()
 
 	// get posts with userID=7
@@ -122,16 +130,16 @@ func main() {
 		log.Println(err)
 		os.Exit(ERR_HTTP_GET)
 	}
-
-	// close http.response.body
 	defer resp.Body.Close()
 
+	// read body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
 		os.Exit(ERR_HTTP_BODY)
 	}
 
+	// json to struct
 	var posts []Post
 	err = json.Unmarshal(body, &posts)
 	if err != nil {
@@ -139,12 +147,30 @@ func main() {
 		os.Exit(ERR_JSON_UNMARSHAL)
 	}
 
-	var wgPosts sync.WaitGroup
+	// sqlPreparePostSave
+	qs := "INSERT INTO posts (id, userid, title, body) VALUES (?, ?, ?, ?)"
+	stmtPostSave, err := db.Prepare(qs)
+	if err != nil {
+		log.Println(err)
+		os.Exit(ERR_DB_STMT)
+	}
+	defer stmtPostSave.Close()
+
+	// sqlPrerareCommentSave
+	qs = "INSERT INTO comments (id, postid, name, email, body) VALUES (?, ?, ?, ?, ?)"
+	stmtCommentSave, err := db.Prepare(qs)
+	if err != nil {
+		log.Println(err)
+		os.Exit(ERR_DB_STMT)
+	}
+	defer stmtCommentSave.Close()
 
 	// write posts to database
+	var wgPosts sync.WaitGroup
 	for _, post := range posts {
 		wgPosts.Add(1)
-		go writePostToDB(post, wgPosts)
+		go writePostToDB(post, stmtPostSave, stmtCommentSave, wgPosts)
 	}
+
 	wgPosts.Wait()
 }
